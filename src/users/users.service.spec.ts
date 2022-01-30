c817c72fb057f9f20509e228de8f002f3ee5cea2
jongminshin <ace4463@gmail.com>
2022-01-30 13:30:44 +0900

    test: users.service.spec - defined

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,28 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {UsersService} from './users.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { BcryptService } from '../common/bcrypt/bcrypt.service';
+import { UserEntity } from '../entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const MockUserRepository = () => {};
+const MockBcryptService = () => {};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: MockUserRepository,
+        },
+        {
+          provide: BcryptService,
+          useValue: MockBcryptService,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
